9b69ab6253a5f9a4181bdd87462287b2e702aef1
laneyvlaneyv <131590151+laneyvlaneyv@users.noreply.github.com>
2024-07-28 21:54:17 +0300

    linux

---
+++ Builds/LinuxMakefile/pre_build/juce_LinuxSubprocessHelperBinaryData.h
@@ -0,0 +1,10 @@
+/* (Auto-generated binary data file). */
+
+#pragma once
+
+namespace LinuxSubprocessHelperBinaryData
+{
+    extern const char*  juce_linux_subprocess_helper;
+    const int           juce_linux_subprocess_helperSize = 16024;
+}
+
